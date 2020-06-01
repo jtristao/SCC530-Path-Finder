00337c4d46b728c749be93d8354e9d7791cc9cca
João Vitor Tristão <joaovitortristao@gmail.com>
2020-06-01 16:29:46 -0300

    Reorganizando arquivos + salvando videos no lugar certo

--- Maze/maze.py
+++ Maze/maze.py
@@ -26,28 +26,28 @@ def main():
 	# DFS
 	print("Building DFS...")
 	path, min_path = Algorithm.depth_first_search(maze)
-	Animation.animate_path(path, min_path, (255,255,51), maze, "dfs.avi")
+	Animation.animate_path(path, min_path, (255,255,51), maze, "Videos/depth_first_search.avi")
 
 	# BFS
 	print("Building Breadth First Search...")
 	path, min_path = Algorithm.breadth_first_search(maze)
-	Animation.animate_path(path, min_path, (255,255,51), maze, "bfs.avi")
+	Animation.animate_path(path, min_path, (255,255,51), maze, "Videos/breadth_first_search.avi")
 
 	# Best Search First
 	print("Building Best First Search...")
 	path, min_path = Algorithm.best_first_search(maze)
-	Animation.animate_path(path, min_path, (255,255,51), maze, "best.avi")
+	Animation.animate_path(path, min_path, (255,255,51), maze, "Videos/best_first_search.avi")
 	
 	# A*
 	print("Building A* Search...")
 	path, min_path = Algorithm.a_star_search(maze)
-	Animation.animate_path(path, min_path, (255,255,51), maze, "star.avi")
+	Animation.animate_path(path, min_path, (255,255,51), maze, "Videos/a_star_search.avi")
 
 
 	# Hill Climbing*
 	print("Building Hill Climbing Search...")
 	path, min_path = Algorithm.hill_climbing_search(maze)
-	Animation.animate_path(path, min_path, (255,255,51), maze, "hill.avi")
+	Animation.animate_path(path, min_path, (255,255,51), maze, "Videos/hill_climbing_search.avi")
 
 
 if __name__ == '__main__':
